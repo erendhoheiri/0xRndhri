b57bc07fe485dba7df8156e4eee83fc5d46c6430
Eren Dhoheiri <eren.dhoheiri@efishery.com>
2023-09-30 20:48:14 +0700

    fix maps

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -18,12 +18,16 @@ const SEO = ({ title, description }) => {
     title: title
       ? `${title} - ${site.siteMetadata.title}`
       : site.siteMetadata.title,
-    description: description || site.siteMetadata.description,
+    description: description || site.siteMetadata.description
   };
 
   return (
     <Helmet title={seo.title}>
-      <meta name="description" content={seo.description} />
+      <meta name='description' content={seo.description} />
+      <meta
+        http-equiv='Content-Security-Policy'
+        content="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;"
+      />
     </Helmet>
   );
 };
