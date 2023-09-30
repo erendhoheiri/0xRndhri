ab59e7fbdcf7e9492366f043233aaf6fcd84ee5a
Eren Dhoheiri <eren.dhoheiri@efishery.com>
2023-09-30 13:24:10 +0700

    fix size button

--- web/src/styles/ShareButton.js
+++ web/src/styles/ShareButton.js
@@ -4,4 +4,19 @@ export const ShareButtonStyle = styled.div`
   -moz-appearance: none;
   -webkit-appearance: none;
   appearance: none;
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+  }
+  @media (min-width: 481px) and (max-width: 767px) {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+  }
+  @media (min-width: 320px) and (max-width: 480px) {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+  }
 `;
