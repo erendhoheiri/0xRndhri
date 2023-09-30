46d0449b72e8e0e4dee09772d860f265f75e9a23
Eren Dhoheiri <eren.dhoheiri@efishery.com>
2023-09-30 13:10:26 +0700

    fix button

--- web/src/components/ShareButtons.js
+++ web/src/components/ShareButtons.js
@@ -16,7 +16,7 @@ const ShareButtons = ({ title, url, twitterHandle, tags }) => {
   return (
     <ShareButtonStyle>
       <FacebookShareButton url={url}>
-        <FacebookIcon size={32} round={true} />
+        <FacebookIcon size={40} round={true} />
       </FacebookShareButton>
 
       <TwitterShareButton
@@ -25,15 +25,15 @@ const ShareButtons = ({ title, url, twitterHandle, tags }) => {
         via={twitterHandle}
         hashtags={tags}
       >
-        <TwitterIcon size={32} round={true} />
+        <TwitterIcon size={40} round={true} />
       </TwitterShareButton>
 
       <RedditShareButton url={url} title={title}>
-        <RedditIcon size={32} round={true} />
+        <RedditIcon size={40} round={true} />
       </RedditShareButton>
 
       <WhatsappShareButton url={url} title={title}>
-        <WhatsappIcon size={32} round={true} />
+        <WhatsappIcon size={40} round={true} />
       </WhatsappShareButton>
     </ShareButtonStyle>
   );
